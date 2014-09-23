df68884308c9270f4d0c1108019b54d4fec60fea
David Sabalete <dsabalete@gmail.com>
2014-09-23 10:59:16 +0200

    ex5 modified

--- lpthw/ex5.py
+++ lpthw/ex5.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
 # http://learnpythonthehardway.org/book/ex5.html
 
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+unit_height = ["cm", "inches"]
+unit_weight = ["kg", "pounds"]
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+unit = 1
+
+if (unit == 0):
+	height = height * 2.54 # cm
+	weight = weight * 0.45359237 # kg
+	
+
+print "Let's talk about %s." % name
+print "He's %d %s tall." % (height, unit_height[unit]) # inches or cm
+print "He's %d %s heavy." % (weight, unit_weight[unit]) # pounds or kg
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    age, height, weight, age + height + weight)
