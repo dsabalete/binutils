fea81ab20de9f542262c692d8e4eb4c2ad9b1eaf
dsabalete <dsabalete@gmail.com>
2014-09-30 07:09:53 +0000

    Update ex17.py

--- lpthw/ex17.py
+++ lpthw/ex17.py
@@ -1,2 +1,31 @@
 # -*- coding: utf-8 -*-
 # http://learnpythonthehardway.org/book/ex17.html
+
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+#print "Copying from %s to %s" % (from_file, to_file)
+
+# we could do these two on one line, how?
+#in_file = open(from_file)
+#indata = in_file.read()
+#indata = open(from_file).read()
+
+#print "The input file is %d bytes long" % len(indata)
+
+#print "Does the output file exist? %r" % exists(to_file)
+#print "Ready, hit RETURN to continue, CTRL-C to abort."
+#raw_input()
+
+#out_file = open(to_file, 'w')
+#out_file.write(indata)
+
+# All the stuff in one line
+open(to_file, 'w').write(open(from_file).read())
+
+print "Alright, all done."
+
+#out_file.close()
+#in_file.close()
