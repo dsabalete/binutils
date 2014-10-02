3f79c8499ebacde993e99f1aa15b5bad5fbb6961
dsabalete <dsabalete@gmail.com>
2014-10-02 07:56:57 +0000

    ex23.py updated

--- lpthw/ex23.py
+++ lpthw/ex23.py
@@ -2,3 +2,22 @@
 # http://learnpythonthehardway.org/book/ex23.html
 
 # NO CODING IN THIS EXERCISE
+
+### READ SOME CODE
+
+# 1. Finding Python source code for things you need.
+# 2. Reading through the code and looking for files.
+# 3. Trying to understand code you find.
+
+# Here's what you do:
+# 1. Go to bitbucket.org, github.com, or gitorious.org with your favorite web browser and serach for "python".
+# 2. Pick a random project and click on it.
+# 3. Click on the Source tab and browse through the list fo files and directories until you find a .py file.
+# 4. Start at the top and read through the code, taking notes on what you think it does.
+# 5. If any symbols or strange words seem to interest you, write them down to research later.
+
+# Other sites with source code:
+# - github.com
+# - launchpad.net
+# - gitorious.org
+# - sourceforge.net
