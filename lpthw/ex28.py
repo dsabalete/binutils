465b875a1e36346f39da94352d7ad564d82f1ac0
dsabalete <dsabalete@gmail.com>
2014-10-03 12:45:36 +0000

    ex28.py modified

--- lpthw/ex28.py
+++ lpthw/ex28.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 # http://learnpythonthehardway.org/book/ex28.html
 
+True and True   # True
+False and True  # False
+1 == 1 and 2 == 1 # False
+"test" == "test" # True
+1 == 1 or 2 != 1 # True
+True and 1 == 1 # True
+False and 0 != 0 # False
+True or 1 == 1 # True
+"test" == "testing" # False
+1 != 0 and 2 == 1 # False
