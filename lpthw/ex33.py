0a4727092feb5889a49232746926d7b873154f5c
dsabalete <dsabalete@gmail.com>
2014-10-06 08:59:27 +0000

    ex22.py with for and range

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 # http://learnpythonthehardway.org/book/ex33.html
 
-i = 0
-numbers = []
+def runNumbers(n, inc):
+    i = 0
+    numbers = []
+    while i < n:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        
+        i = i + inc
+        print "Numbers now ", numbers
+        print "At the bottom i is %d" % i
+        
+    print "The numbers: "
+    
+    for num in numbers:
+        print num    
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-    
-    i = i + 1
-    print "Numbers now ", numbers
-    print "At the bottom i is %d" % i
-    
-print "The numbers: "
 
-for num in numbers:
-    print num
+def runNumbersFor(n, inc):
+    numbers = []
+    for i in range(0, n, inc):
+        print "At the top i is %d" % i
+        numbers.append(i)
+        print "Numbers now ", numbers
+        print "At the bottom i is %d" % i
+        
+    print "The numbers: "
     
+    for num in numbers:
+        print num    
+        
+        
+# runNumbers(10, 2)
+runNumbersFor(10, 2)
