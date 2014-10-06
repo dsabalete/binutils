87715182ff66dd269028dc9863fb48a7b3a6d574
dsabalete <dsabalete@gmail.com>
2014-10-06 09:16:07 +0000

    ex34.py updated

--- lpthw/ex34.py
+++ lpthw/ex34.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 # http://learnpythonthehardway.org/book/ex34.html
 
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+ordinal = ['first', 'second', 'third', 'fourth', 'fiveth', 'sixth']
+
+def printAnimal(n):
+    print "The %s animal is at %d and is a %s." % (ordinal[n], n, animals[n]),
+    print "The animal at %d is the %s animal and is a %s." % (n, ordinal[n], animals[n])
+    
+printAnimal(1)
+printAnimal(2)
+printAnimal(0)
+printAnimal(3)
+printAnimal(4)
+printAnimal(2)
+printAnimal(5)
+printAnimal(4)
