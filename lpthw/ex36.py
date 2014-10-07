600a983072114565808aefe79e0e429d60180551
dsabalete <dsabalete@gmail.com>
2014-10-07 11:02:13 +0000

    ex36 updated

--- lpthw/ex36.py
+++ lpthw/ex36.py
@@ -1,12 +1,34 @@
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
 # http://learnpythonthehardway.org/book/ex36.html
 
-import sys from exit
+from sys import exit
 
+def puertaA():
+    print "puerta A"
+
+
+def puertaB():
+    print "puerta B"
+
+
+def fin(msg):
+    print msg, "Buen trabajo!"
+    exit(0)
 
 
 def start():
     print "Estas en una habitacion oscura"
+    print "Hay dos puertas: A - blanca y B - negra"
+    print "Que puerta escojeras?"
+    
+    choice = raw_input("> ")
+    
+    if choice == "A":
+        puertaA()
+    elif choice == "B":
+        puertaB()
+    else:
+        fin("Te quedas dando vueltas en la habitacion hasta que mueres.")
     
     
 start()
