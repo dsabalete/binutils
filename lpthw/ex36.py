4a3e6cef5125c3fc981dda75f0528a04eb228681
dsabalete <dsabalete@gmail.com>
2014-10-07 10:38:49 +0000

    some changes

---
+++ lpthw/ex36.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# http://learnpythonthehardway.org/book/ex36.html
+
+import sys from exit
+
+
+
+def start():
+    print "Estas en una habitacion oscura"
+    
+    
+start()
