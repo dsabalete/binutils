8add67a8b72e6f71ec5e37e67277568c0a361ff5
David Sabalete <dsabalete@gmail.com>
2016-06-17 12:41:10 +0200

    Create setup-angular.sh

---
+++ setup-angular.sh
@@ -0,0 +1,34 @@
+npm install bower
+bower install bootstrap
+#grunt utils
+npm install -g grunt-cli
+npm install grunt --save-dev
+npm install grunt-contrib-jshint --save-dev
+npm install jshint-stylish --save-dev
+npm install time-grunt --save-dev
+npm install jit-grunt --save-dev
+npm install grunt-contrib-copy --save-dev 
+npm install grunt-contrib-clean
+npm install grunt-contrib-concat --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-filerev --save-dev
+npm install grunt-usemin --save-dev
+npm install grunt-contrib-watch --save-dev
+npm install grunt-contrib-connect --save-dev
+# gulp utils
+npm install -g gulp
+npm install gulp --save-dev
+npm install gulp-jshint --save-dev
+npm install jshint-stylish --save-dev
+npm install gulp-imagemin --save-dev 
+npm install gulp-concat --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-clean-css --save-dev
+npm install gulp-usemin --save-dev
+npm install gulp-cache --save-dev
+npm install gulp-rev --save-dev
+npm install gulp-rename --save-dev
+npm install gulp-notify --save-dev
+npm install browser-sync --save-dev
+npm install del --save-dev
